10fc5ef93762eb9930947202fb7c8bfc76cd1a42
ficus <j307582@gmail.com>
2024-07-09 18:42:23 +0800

    first commit

---
+++ src/main/java/eassue/question/model/question.java
@@ -0,0 +1,27 @@
+package eassue.question.model;
+
+public class question {
+	private String title;
+	private String article;
+	private Integer memberId;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArticle() {
+		return article;
+	}
+	public void setArticle(String article) {
+		this.article = article;
+	}
+	public Integer getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+	
+	
+}
